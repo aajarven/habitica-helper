89201ce976c0ff1f5f764731c1b9895d9dc970ff
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-02-22 11:50:33 +0200

    Add stuff needed for constructing request headers.

---
+++ conf/header.py
@@ -0,0 +1,11 @@
+"""
+The static header info Habitica requires.
+"""
+from conf.secrets import PLAYER_USER_ID, PLAYER_API_TOKEN
+
+HEADER = {
+    "x-client":
+        "f687a6c7-860a-4c7c-8a07-9d0dcbb7c831-habitica-challenge-helper",
+    "x-api-user": PLAYER_USER_ID,
+    "x-api-key":  PLAYER_API_TOKEN,
+}
