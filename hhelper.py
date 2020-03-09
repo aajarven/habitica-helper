70f6eff46915c5ad2da5e32d05f1f2362e494cd0
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-03-09 21:00:24 +0200

    Make winner picking actually pick a winner.

--- hhelper.py
+++ hhelper.py
@@ -3,10 +3,12 @@ Scripts for automating actions related to Habitica.
 """
 
 from __future__ import print_function
+import datetime
 import click
 
 from conf.header import HEADER
 from src.habiticatool import PartyTool
+from src.stockrandomizer import StockRandomizer
 
 @click.group()
 def cli():
@@ -18,12 +20,15 @@ def cli():
 @cli.command()
 def sharing_winners():
     """
-    Print nicks of all users who are eligible winners.
+    Pick winner from amongst all users who are eligible winners.
 
     This tool examines all participants in the newest sharing challenge, and
     prints ones that could win it. The newest challenge is picked automatically
     based on creation date, and everyone who has completed all todos is
     considered eligible to win.
+
+    The winner is chosen based on the opening, closing, highest and lowest
+    values of AEX index (Amsterdam Stock Exchange) from the previous monday.
     """
     tool = PartyTool(HEADER)
     challenge = tool.current_sharing_weekend()
@@ -31,9 +36,18 @@ def sharing_winners():
     completers = tool.eligible_winners(challenge["id"], participants)
 
     click.echo("Eligible winners for challenge \"{}\" are:"
-               "".format(challenge["name"]))
+               "".format(challenge["name"].encode("utf-8")))
     for nick in completers:
         click.echo(nick)
+    click.echo("")
+
+    today = datetime.date.today()
+    last_monday = today - datetime.timedelta(today.weekday())
+    click.echo("Using stock data from {}".format(last_monday))
+
+    rand = StockRandomizer("^AEX", last_monday)
+    winner_index = rand.pick_integer(0, len(completers))
+    click.echo("{} wins the challenge!".format(completers[winner_index]))
 
 if __name__ == "__main__":
     cli()
