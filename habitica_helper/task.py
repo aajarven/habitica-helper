57888cda958ccf2b95301fd54ff22a4147ecc259
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-05-22 18:55:48 +0000

    Make the task API compatible

--- habitica_helper/task.py
+++ habitica_helper/task.py
@@ -11,6 +11,13 @@ class Task():
     A Habitica task: can be either habit, daily or a todo.
     """
 
+    # Counterparts for variables in Habitica API where the naming differs.
+    habitica_keys = {
+        "uppable": "up",
+        "downable": "down",
+        "difficulty": "priority",
+    }
+
     def __init__(self, task_data):
         """
         Create a new task instance based on the given data.
@@ -24,19 +31,22 @@ class Task():
         notes: A longer description for the task (str)
         date: A due date for the task. Either a timestamp string in format
               "yyyy-mm-dd" or an instance of date/datetime.
+        frequency: How often the streak is reset. Possible values are "daily",
+                   "weekly", "monthly" and "yearly"
         difficulty: The difficulty of the task. Values "trivial", "easy",
                     "medium" and "hard" are supported. If no value is given,
                     the task is created as an easy one.
-        uppable: A boolean determining whether the '+' of a habit is active
-        downable: A boolean determining whether the '-' of a habit is active
+        uppable: Whether the '+' of a habit is active. "true" or "false".
+        downable: Whether the '-' of a habit is active. "true" or "false"
         """
         self.text = task_data.get("text", None)
         self.tasktype = task_data.get("tasktype", None)
         self.notes = task_data.get("notes", "")
         self.date = task_data.get("date", None)
+        self.frequency = task_data.get("frequency", None)
         self.difficulty = task_data.get("difficulty", "easy")
-        self.uppable = task_data.get("uppable", True)
-        self.downable = task_data.get("downable", False)
+        self.uppable = task_data.get("uppable", "true")
+        self.downable = task_data.get("downable", "false")
 
     def create_to_challenge(self, challenge_id, header):
         """
@@ -56,9 +66,10 @@ class Task():
         Return this task in the standard Habitica API form.
         """
         datadict = {"text": self.text, "type": self.tasktype}
-        for key in ["notes", "date", "difficulty", "uppable", "downable"]:
+        for key in ["notes", "date", "difficulty", "uppable", "downable",
+                    "frequency"]:
             if getattr(self, key) not in [None, ""]:
-                datadict[key] = getattr(self, key)
+                datadict[self.habitica_keys.get(key, key)] = getattr(self, key)
         return datadict
 
     @property
@@ -138,21 +149,48 @@ class Task():
                              "".format(type(date)))
 
     @property
+    def frequency(self):
+        """
+        How often the streak is reset.
+
+        Possible values are "daily", "weekly", "monthly" or "yearly".
+        """
+        return self._frequency
+
+    @frequency.setter
+    def frequency(self, frequency):
+        if frequency not in ["daily", "weekly", "monthly", "yearly", None]:
+            raise ValueError("Illegal frequency value {} encountered."
+                             "".format(frequency))
+        self._frequency = frequency
+
+    @property
     def difficulty(self):
         """
         Difficulty of the task.
 
-        Allowed values are "trivial", "easy", "medium" and "hard".
+        Allowed input values are "trivial", "easy", "medium" and "hard", or the
+        corresponding numeric values 0.1, 1, 1.5 and 2. The value is returned
+        as the numeric value.
         """
         return self._difficulty
 
     @difficulty.setter
     def difficulty(self, difficulty):
-        if difficulty.lower() not in ["trivial", "easy", "medium", "hard"]:
+        if difficulty in [0.1, 1, 1.5, 2]:
+            self._difficulty = difficulty
+        elif difficulty.lower() in ["trivial", "easy", "medium", "hard"]:
+            difficulties = {
+                "trivial": 0.1,
+                "normal": 1,
+                "medium": 1.5,
+                "hard": 2,
+                }
+            self._difficulty = difficulties[difficulty.lower()]
+        else:
             raise ValueError("Illegal task difficulty {} encountered. Allowed "
                              "values are 'trivial', 'easy', 'medium' and "
                              "'hard'".format(difficulty))
-        self._difficulty = difficulty
 
     @property
     def uppable(self):
@@ -163,12 +201,14 @@ class Task():
 
     @uppable.setter
     def uppable(self, uppable):
-        if uppable is not None and not isinstance(uppable, bool):
+        if isinstance(uppable, bool):
+            self._uppable = str(uppable).lower()
+        elif uppable not in [None, "True", "False", "true", "false"]:
             raise ValueError("Illegal value encountered when setting whether "
-                             "a task should have the '+' enabled: must be a "
-                             "boolean but {} was encountered."
+                             "a task should have the '+' enabled: must be "
+                             "either 'true' or 'false' but {} was encountered."
                              "".format(type(uppable)))
-        self._uppable = uppable
+        self._uppable = uppable.lower()
 
     @property
     def downable(self):
@@ -179,9 +219,9 @@ class Task():
 
     @downable.setter
     def downable(self, downable):
-        if downable is not None and not isinstance(downable, bool):
+        if downable not in [None, "True", "False", "true", "false"] and not isinstance(downable, bool):
             raise ValueError("Illegal value encountered when setting whether "
                              "a task should have the '+' enabled: must be a "
                              "boolean but {} was encountered."
                              "".format(type(downable)))
-        self._downable = downable
+        self._downable = downable.lower()
